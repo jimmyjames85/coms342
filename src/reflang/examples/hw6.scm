80a093f63b3d81441741b621db456ad491d11951
BWAYNE <b@t.man>
2015-03-03 19:22:38 -0600

    answer questions in hw6

---
+++ src/reflang/examples/hw6.scm
@@ -0,0 +1,8 @@
+HW6 #1
+
+A The enviornment frame is ExtendEnvRec
+	
+B Originally it did not return the same FunVal
+
+C I've modified the function to used the new ExtendEnvRec
+  and now it DOES return the same FunVal object each time
