62fc2de08ee83560f5cd87d980011460e9752557
jmt <jmt>
2015-03-09 19:09:56 -0500

    HW7 begin

--- src/reflang/Env.java
+++ src/reflang/Env.java
@@ -10,125 +10,63 @@ import reflang.Value.*;
  * @author hridesh
  *
  */
-public interface Env
-{
-	RefVal get(String search_var);
-
+public interface Env {
+	Value get (String search_var);
 	boolean isEmpty();
 
 	@SuppressWarnings("serial")
-	static public class LookupException extends RuntimeException
-	{
-		LookupException(String message)
-		{
+	static public class LookupException extends RuntimeException {
+		LookupException(String message){
 			super(message);
 		}
 	}
-
-	static public class EmptyEnv implements Env
-	{
-		public RefVal get(String search_var)
-		{
+	
+	static public class EmptyEnv implements Env {
+		public Value get (String search_var) {
 			throw new LookupException("No binding found for name: " + search_var);
 		}
-
-		public boolean isEmpty()
-		{
-			return true;
-		}
+		public boolean isEmpty() { return true; }
 	}
-
-	static public class ExtendEnv implements Env
-	{
-		private Env _saved_env;
-		private String _var;
-		//private Value _val;
-		private int _refLoc;
-
-		public ExtendEnv(Env saved_env, String var, RefVal val)
-		{
+	
+	static public class ExtendEnv implements Env {
+		private Env _saved_env; 
+		private String _var; 
+		private Value _val; 
+		public ExtendEnv(Env saved_env, String var, Value val){
 			_saved_env = saved_env;
 			_var = var;
-			_refLoc = val.loc();
-			//_val = val;
+			_val = val;
 		}
-
-		public synchronized RefVal get(String search_var)
-		{
+		public synchronized Value get (String search_var) {
 			if (search_var.equals(_var))
-				return new RefVal(_refLoc);
-
+				return _val;
 			return _saved_env.get(search_var);
 		}
-
-		public boolean isEmpty()
-		{
-			return false;
-		}
-
-		public Env saved_env()
-		{
-			return _saved_env;
-		}
-
-		public String var()
-		{
-			return _var;
-		}
-
-		public RefVal val()
-		{
-			return new RefVal(_refLoc);
-		}
+		public boolean isEmpty() { return false; }
+		public Env saved_env() { return _saved_env; }
+		public String var() { return _var; }
+		public Value val() { return _val; }
 	}
 
-	static public class ExtendEnvRec implements Env
-	{
+	static public class ExtendEnvRec implements Env {
 		private Env _saved_env;
 		private List<String> _names;
 		private List<Value.FunVal> _funs;
-
-		private Heap recHeap = new Heap.Heap16Bit();
-
-		public Env saved_env()
-		{
-			return _saved_env;
-		}
-
-		public List<String> names()
-		{
-			return _names;
-		}
-
-		public List<FunVal> vals()
-		{
-			return _funs;
-		}
-
-		public ExtendEnvRec(Env saved_env, List<String> names, List<Value.FunVal> funs)
-		{
+		public Env saved_env() { return _saved_env; }
+		public List<String> names() { return _names; }
+		public List<FunVal> vals() { return _funs; }
+		public ExtendEnvRec(Env saved_env, List<String> names, List<Value.FunVal> funs){
 			_saved_env = saved_env;
 			_names = names;
 			_funs = funs;
 		}
-
-		public boolean isEmpty()
-		{
-			return false;
-		}
-
-		public RefVal get(String search_var)
-		{
-
+		public boolean isEmpty() { return false; }
+		public Value get (String search_var) {
 			int size = _names.size();
-			for (int index = 0; index < size; index++)
-			{
-				if (search_var.equals(_names.get(index)))
-				{
+			for(int index = 0; index < size; index++) {
+				if (search_var.equals(_names.get(index))) {
 					FunVal f = _funs.get(index);
-					
-					return (RefVal)recHeap.ref((new Value.FunVal(this, f.formals(), f.body())));
-					//	return new Value.FunVal(this, f.formals(), f.body());
+					return new Value.FunVal(this, f.formals(), f.body());				
 				}
 			}
 			return _saved_env.get(search_var);
--- src/reflangHW6P5/Heap.java
+++ src/reflangHW6P5/Heap.java
@@ -6,57 +6,74 @@ package reflang;
  * @author hridesh
  *
  */
-public interface Heap {
+public interface Heap
+{
 
-	Value ref (Value value);
+	Value ref(Value value);
 
-	Value deref (Value.RefVal loc);
+	Value deref(Value.RefVal loc);
 
-	Value setref (Value.RefVal loc, Value value);
+	Value setref(Value.RefVal loc, Value value);
 
-	Value free (Value.RefVal value);
+	Value free(Value.RefVal value);
 
-	static public class Heap16Bit implements Heap {
+	static public class Heap16Bit implements Heap
+	{
 		static final int HEAP_SIZE = 65_536;
-		
+
 		Value[] _rep = new Value[HEAP_SIZE];
 		int index = 0;
-		
-		public Value ref (Value value) {
-			if(index >= HEAP_SIZE)
+
+		public Value ref(Value value)
+		{
+			if (index >= HEAP_SIZE)
 				return new Value.DynamicError("Out of memory error");
 			Value.RefVal new_loc = new Value.RefVal(index);
 			_rep[index++] = value;
 			return new_loc;
 		}
 
-		public Value deref (Value.RefVal loc) {
-			try {
+		public Value deref(Value.RefVal loc)
+		{
+			try
+			{
 				return _rep[loc.loc()];
-			} catch (ArrayIndexOutOfBoundsException e) {
+			}
+			catch (ArrayIndexOutOfBoundsException e)
+			{
 				return new Value.DynamicError("Segmentation fault at access " + loc);
 			}
 		}
 
-		public Value setref (Value.RefVal loc, Value value) {
-			try {
+		public Value setref(Value.RefVal loc, Value value)
+		{
+			try
+			{
 				return _rep[loc.loc()] = value;
-			} catch (ArrayIndexOutOfBoundsException e) {
+			}
+			catch (ArrayIndexOutOfBoundsException e)
+			{
 				return new Value.DynamicError("Segmentation fault at access " + loc);
 			}
 		}
 
-		public Value free (Value.RefVal loc) {
-			try {
+		public Value free(Value.RefVal loc)
+		{
+			try
+			{
 				_rep[loc.loc()] = null;
 				//REMARK: students should add this location to free list.
 				return loc;
-			} catch (ArrayIndexOutOfBoundsException e) {
+			}
+			catch (ArrayIndexOutOfBoundsException e)
+			{
 				return new Value.DynamicError("Segmentation fault at access " + loc);
 			}
 		}
 
-		public Heap16Bit(){}
+		public Heap16Bit()
+		{
+		}
 	}
 
 }
